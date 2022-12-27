6f609cabdcbe3d163e20d40e78b5b95b3e47c952
MastyDev <mastydev@gmail.com>
2022-12-27 21:26:51 +0100

    main

--- gptts.py
+++ gptts.py
@@ -11,8 +11,8 @@ openai.api_key = os.environ["OPENAI_API_KEY"] # OpenAI API
 pygame.init()
 pygame.mixer.init()
 
-def speak(text, language='ja'):
-    tts = gtts.gTTS(text,lang=language,slow=False)
+def speak(text, language='it'):
+    tts = gtts.gTTS(text,lang=language,tld="co.uk", slow=False)
     tts.save('_temp3.mp3')
     pygame.mixer.music.load("_temp3.mp3")
     pygame.mixer.music.play()
@@ -20,16 +20,16 @@ def speak(text, language='ja'):
 #input
 # start_sequence = "\nAI:"
 # restart_sequence = "\nHuman: "
-print("chatGPTTS - beta")
+print("chatGPTTS")
 desc=input(f"\nHuman:\n")
 response = openai.Completion.create(
   model="text-davinci-003",
   prompt=desc,
   temperature=0.9,
-  max_tokens=512,
+  max_tokens=1024,
   top_p=1,
-  frequency_penalty=-0.0,
-  presence_penalty=0.7,
+  frequency_penalty=-0.1,
+  presence_penalty=0.75,
 )
 #output
 speak(response.choices[0].text)
